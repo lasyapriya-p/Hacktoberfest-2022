33a0359cea43f40d5d8f3f14b5c40e258bb4d958
Sravanthi <113963634+sravanthimurukonda@users.noreply.github.com>
2022-10-27 10:48:49 +0530

    Create binary.cpp

---
+++ binary.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std;
+
+int binarySearch(int a[], int l, int r, int x);
+int binarySearch(int a[], int l, int r, int x) 
+{ 
+    if (r >= l) { 
+        int mid = l + (r - l) / 2;
+  
+        
+        if (a[mid] == x) 
+            return mid; 
+  
+        
+        if (a[mid] > x) 
+            return binarySearch(a, l, mid - 1, x); 
+   
+        return binarySearch(a, mid + 1, r, x); 
+    }   
+    return -1; 
+}
